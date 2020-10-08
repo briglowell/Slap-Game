6f87fdee9d2daa2dc77590df0dd274be6dee93bc
Brig Lowell <brig.lowell@gmail.com>
2020-10-08 13:25:35 -0600

    asdf

--- game.js
+++ game.js
@@ -8,28 +8,40 @@
 //
 
 var health = 100;
-let monsterName = "";
+let monsterName = "fluffy";
 let monsterHitCount = 0;
-let playerName = '';
+let playerName = "excalibur";
 
 function slap() {
   health--;
+  monsterHitCount++;
   drawHealth()
+  drawHits()
 }
 
 function punch() {
   health -= 5;
+  monsterHitCount++;
   drawHealth()
+  drawHits()
 }
 
 function kick() {
   health -= 10;
+  monsterHitCount++;
   drawHealth()
+  drawHits()
 }
 
 function ultimatePunch() {
   health -= 20; //two punches with a kick
+  monsterHitCount++;
   drawHealth()
+  drawHits()
+}
+
+function update() {
+
 }
 
 function drawHealth() {
@@ -37,6 +49,18 @@ function drawHealth() {
   charHealth.innerText = health.toString();
 }
 
-function drawName() {}
+function drawName() {
+  let villianName = document.getElementById('monster-name');
+  let player = document.getElementById('player-name');
+  villianName.innerText = monsterName;
+  player.innerText = playerName;
+}
+
+function drawHits() {
+  let hit = document.getElementById('monster-hit-count');
+  hit.innerText = monsterHitCount.toString();
+}
 
 drawHealth()
+drawName()
+drawHits()
